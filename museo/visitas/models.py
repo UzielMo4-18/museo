6054b35b0a1fa926d68e912a0ce378aa0fa94eb8
UzielM <uzielmoreno18@gmail.com>
2021-03-24 13:18:51 -0600

    [UPDATE] Modelos actualizados, timestamp automatico y campos opcionales

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Visita(models.Model):
+    email=models.EmailField
+    timestamp_in=models.DateTimeField(auto_now_add=True)
+    timestamp_out=models.DateTimeField(blank=True)
+    comment=models.CharField(max_length=140,blank=True)
