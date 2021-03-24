7d2a7cac0cbf04ccd7597b1a5a47e0e039513bf6
UzielM <uzielmoreno18@gmail.com>
2021-03-24 13:49:16 -0600

    [UPDATE] Correccion de los modelos y sus campos

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Visita(models.Model):
-    email=models.EmailField
+    email=models.EmailField()
     timestamp_in=models.DateTimeField(auto_now_add=True)
-    timestamp_out=models.DateTimeField(blank=True)
+    timestamp_out=models.DateTimeField(blank=True,null=True)
     comment=models.CharField(max_length=140,blank=True)
